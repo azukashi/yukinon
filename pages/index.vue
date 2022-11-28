e8c291d691757d33601774ba516f57c6f1d11d02
Gifaldy Azka <me@falcxxdev.ml>
2022-11-28 19:05:51 +0700

    chore: content update & use Head component

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,27 @@
 <template>
+    <Head>
+        <Title>猫乃ユキノ Web</Title>
+        <Meta name="description" value="猫乃ユキノ Web with Nuxt 3 & Tailwind CSS" />
+    </Head>
+
     <div class="mx-5 font-lexend">
         <div
-            class="rounded-xl shadow-xl p-10 mt-16 mx-auto max-w-xl text-center backdrop-blur-md bg-[url('https://cdn.upload.systems/uploads/z8qm1sW8.jpg')] bg-contain bg-no-repeat hover:border-2 hover:border-sky-300 hover:border-dashed hover:-translate-y-3 transition-all"
+            class="rounded-xl shadow-xl p-10 mt-16 mx-auto max-w-xl text-center backdrop-blur-md bg-[url('https://cdn.upload.systems/uploads/z8qm1sW8.jpg')] bg-contain bg-no-repeat border-2 border-sky-300 border-dashed hover:border-indigo-400 hover:-translate-y-3 transition-all"
         >
-            <h2 class="text-2xl font-bold text-slate-700">Development mode</h2>
+            <h2 class="text-2xl font-bold text-slate-700"><Icon name="🌨" /> Development mode</h2>
             <p class="mt-4 text-lg text-slate-600">Sorry, this website is still in development progress.</p>
             <p class="mt-4 text-md text-slate-600 text-left">
-                If you want to see development version, please visit
-                <code class="text-sky-600">next</code> branch on
-                <a href="https://github.com/gifaldyazkaa/yukinon" class="link">GitHub Repository</a>.
+                If you want to see development version, please visit preview deployment on
+                <a href="https://next-yukinon.falcxxdev.ml" class="link"
+                    ><code class="text-sky-600">next</code> version</a
+                >.
             </p>
             <p class="mt-1 text-md text-slate-600 text-left">
                 Stay up-to-date until stable release by watching the repository!
             </p>
             <p class="mt-8 text-[11px] text-slate-500">
-                <a href="https://twitter.com/nekonoyukin0" class="link">Nekono Yukino</a> Project by
-                <a href="https://falcxxdev.ml" class="link">Falcxxdev</a> with
+                <a href="https://twitter.com/nekonoyukin0" class="link">猫乃ユキノ</a> Project by
+                <a href="https://www.falcxxdev.ml" class="link">Falcxxdev</a> with
                 <a href="https://nuxt.com" class="link">Nuxt 3</a>
             </p>
         </div>
