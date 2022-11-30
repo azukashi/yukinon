47a6341538e8409d9a01ab68933698840ac78957
Gifaldy Azka <me@falcxxdev.ml>
2022-11-30 19:51:50 +0700

    feat: add default layout & remove main app file

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
-        <NuxtLoadingIndicator />
-        <NuxtPage />
+    <Navigation />
+    <div class="container mx-auto p-4">
+        <slot />
     </div>
 </template>
+
+<script setup lang="ts"></script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@400;600;700&display=swap');
