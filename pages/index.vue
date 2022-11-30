aec5dbd6277ca2aae904f54e7c71ef4ca5777d60
Gifaldy Azka <me@falcxxdev.ml>
2022-11-30 22:18:08 +0700

    refactor: [wip] totally rewrite index page

--- pages/index.vue
+++ pages/index.vue
@@ -5,36 +5,47 @@
     </Head>
 
     <div class="mx-5 font-lexend">
-        <div
-            class="rounded-xl shadow-xl p-10 mt-16 mx-auto max-w-xl text-center backdrop-blur-md bg-[url('https://cdn.upload.systems/uploads/z8qm1sW8.jpg')] bg-contain bg-no-repeat border-2 border-sky-300 border-dashed hover:border-indigo-400 hover:-translate-y-3 transition-all"
-        >
-            <h2 class="text-2xl font-bold text-slate-700"><Icon name="🌨" /> Development mode</h2>
-            <p class="mt-4 text-lg text-slate-600">Sorry, this website is still in development progress.</p>
-            <p class="mt-4 text-md text-slate-600 text-left">
-                If you want to see development version, please visit preview deployment on
-                <a href="https://next-yukinon.falcxxdev.ml" class="link"
-                    ><code class="text-sky-600">next</code> version</a
-                >.
-            </p>
-            <p class="mt-1 text-md text-slate-600 text-left">
-                Stay up-to-date until stable release by watching the repository!
-            </p>
-            <p class="mt-1 text-md text-slate-600 text-left font-bold">
-                🚨 CRITICAL ISSUE:
-                <br />
-                <a href="https://github.com/gifaldyazkaa/yukinon/issues/3" class="link"
-                    >[important] translation help needed #3</a
-                >
-            </p>
-            <p class="mt-8 text-[11px] text-slate-500">
-                <a href="https://twitter.com/nekonoyukin0" class="link">猫乃ユキノ</a> Project by
-                <a href="https://www.falcxxdev.ml" class="link">Falcxxdev</a> with
-                <a href="https://nuxt.com" class="link">Nuxt 3</a>
-            </p>
+        <div class="container px-6 mt-24 max-w-md mx-auto sm:max-w-xl md:max-w-5xl lg:flex lg:max-w-full lg:p-0">
+            <div class="lg:p-12 lg:flex-1">
+                <h1 class="text-4xl font-bold text-slate-800 sm:text-3xl md:text-5xl">Nekono Yukino</h1>
+                <h2 class="mt-2 text-xl font-semibold text-slate-800 sm:mt-2 sm:text-2xl">
+                    A Virtual YouTuber belonging to the VTuber office "Re:AcT"
+                </h2>
+                <p class="mt-2 text-slate-600 sm:text-lg">
+                    Welcome, Kainushi-sama! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure dolores earum
+                    sed suscipit vero totam quis quaerat perferendis voluptatem corporis!
+                </p>
+                <div class="mt-2 sm:mt-6">
+                    <NuxtLink
+                        to="/"
+                        class="inline-block px-5 py-3 bg-gradient-to-r from-pink-400 to-violet-500 hover:from-violet-400 hover:to-sky-500 text-white rounded-lg shadow-lg uppercase font-semibold tracking-wider text-sm sm:text-base absolute bottom-8 right-8 z-10 hover:translate-x-4 transition-all"
+                        ><Icon name="material-symbols:arrow-forward-ios-rounded" size="24"
+                    /></NuxtLink>
+                </div>
+                <img
+                    src="https://cdn.upload.systems/uploads/61rmRLt9.png"
+                    height="500"
+                    width="500"
+                    class="mt-4 sm:mt-6 lg:hidden"
+                />
+            </div>
+            <div class="hidden lg:flex lg:w-1/2">
+                <img
+                    src="https://cdn.upload.systems/uploads/61rmRLt9.png"
+                    class="object-cover rounded-l-3xl absolute top-40 -right-52"
+                    height="500"
+                    width="500"
+                    alt="Ayaya"
+                />
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style type="text/tailwindcss">
+body {
+    @apply md:overflow-hidden lg:overflow-hidden;
+}
+</style>
