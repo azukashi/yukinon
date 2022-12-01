dcf71d99191b054e00f97b9309415aa87a1c670a
Gifaldy Azka <me@falcxxdev.ml>
2022-12-01 18:22:40 +0700

    chore: define config for i18n & strapi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/strapi', '@nuxtjs/i18n', 'nuxt-icon'],
     tailwindcss: {
         configPath: 'tailwind.config.ts',
+    },
+    i18n: {
+        locales: ['en', 'ja-JP'],
+        defaultLocale: 'en',
+    },
+    strapi: {
+        url: 'https://yukinon-api.falcxxdev.ml',
     },
 });
