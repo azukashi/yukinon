8fe72bbc150499de767bb42a79462515a6d10a00
Gifaldy Azka <me@falcxxdev.ml>
2022-12-02 17:38:49 +0700

    fix: remove text-* styling

--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,13 @@
     <div class="font-lexend">
         <div class="container px-6 mt-24 max-w-md mx-auto sm:max-w-xl md:max-w-5xl lg:flex lg:max-w-full lg:p-0">
             <div class="lg:p-12 lg:flex-1">
-                <h1 class="text-4xl font-bold text-slate-800 sm:text-3xl md:text-5xl">
-                    {{ bio.name.first }} {{ bio.name.last }}
-                </h1>
-                <h2 class="mt-2 text-xl font-semibold text-slate-800 sm:mt-2 sm:text-xl">
+                <h1 class="text-4xl font-bold sm:text-3xl md:text-5xl">{{ bio.name.first }} {{ bio.name.last }}</h1>
+                <h2 class="mt-2 text-xl font-semibold sm:mt-2 sm:text-xl">
                     Welcome, Kainushi-sama~!
                     <br />
                     I'm {{ bio.name.first }} {{ bio.name.last }} but you can call me {{ bio.nickname }}!
                 </h2>
-                <p class="mt-2 text-slate-600 sm:text-lg">
+                <p class="mt-2 sm:text-lg">
                     A Virtual YouTuber belonging to the VTuber office "Re:AcT", {{ bio.name.last }} hereeee!
                 </p>
                 <div class="mt-2 sm:mt-6">
