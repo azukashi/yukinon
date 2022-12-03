782b49216f2ba790ef80a9c0fc0554d5d0539409
Gifaldy Azka <me@falcxxdev.ml>
2022-12-03 07:59:18 +0700

    feat: add socials & adjust some stylings

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="font-lexend">
-        <div class="container px-4 mt-20 max-w-md mx-auto sm:max-w-xl md:max-w-5xl lg:flex lg:max-w-full lg:p-0">
+        <div
+            class="container px-4 mt-14 max-w-md mx-auto sm:max-w-xl md:max-w-5xl lg:flex lg:flex-wrap lg:gap-3 lg:justify-evenly lg:max-w-full lg:p-0"
+        >
             <div class="hidden lg:flex lg:w-1/2">
                 <img
                     src="https://cdn.upload.systems/uploads/61rmRLt9.png"
@@ -9,20 +11,27 @@
                     width="500"
                 />
             </div>
-            <div class="lg:p-12 lg:flex-1">
+            <div class="lg:p-8 lg:flex-1">
                 <h2 class="mt-2 text-3xl font-semibold sm:mt-2">Overview</h2>
                 <p class="mt-2 sm:text-lg">{{ oview.content }}</p>
                 <h2 class="mt-2 text-3xl font-semibold sm:mt-2">Friendship</h2>
                 <p class="mt-2 sm:text-lg">{{ frnds.content }}</p>
                 <h2 class="mt-2 text-3xl font-semibold sm:mt-2">Trivia</h2>
-                <ul class="sm:text-lg list-disc" v-for="trv in trvia.content" :key="trv">
-                    <li class="">{{ trv }}</li>
-                </ul>
+                <div class="mt-2 px-4">
+                    <ul class="sm:text-lg list-disc" v-for="trv in trvia.content" :key="trv">
+                        <li class="">{{ trv }}</li>
+                    </ul>
+                </div>
                 <div class="mt-2 sm:mt-6">
                     <NuxtLink
-                        to="/about"
-                        class="inline-block px-5 py-3 bg-gradient-to-r from-pink-400 to-violet-500 hover:from-violet-400 hover:to-sky-500 text-white rounded-lg shadow-lg uppercase font-semibold tracking-wider text-sm sm:text-base fixed bottom-8 right-8 z-10 hover:translate-x-4 transition-all"
+                        :to="localePath('/fanart')"
+                        class="inline-block px-5 py-3 bg-gradient-to-r from-pink-400/[.5] to-violet-500/[.5] hover:from-violet-400/[.5] hover:to-sky-500/[.5] backdrop-blur-sm text-snow-light rounded-lg shadow-lg uppercase font-semibold tracking-wider text-sm sm:text-base fixed bottom-8 right-8 z-10 hover:translate-x-4 transition-all"
                         ><Icon name="material-symbols:arrow-forward-ios-rounded" size="24"
+                    /></NuxtLink>
+                    <NuxtLink
+                        :to="localePath('/')"
+                        class="inline-block px-5 py-3 bg-gradient-to-r from-violet-500/[.5] to-pink-400/[.5] hover:from-red-500/[.5] hover:to-pink-400/[.5] backdrop-blur-sm text-snow-light rounded-lg shadow-lg uppercase font-semibold tracking-wider text-sm sm:text-base fixed bottom-8 left-8 z-10 hover:-translate-x-4 transition-all"
+                        ><Icon name="material-symbols:arrow-back-ios-new-rounded" size="24"
                     /></NuxtLink>
                 </div>
                 <img
@@ -31,6 +40,21 @@
                     width="500"
                     class="mt-20 sm:mt-6 lg:hidden"
                 />
+            </div>
+            <div class="lg:flex-[50%]">
+                <h2 class="mt-2 text-3xl font-semibold sm:mt-2">Socials</h2>
+                <p class="mt-2 sm:text-lg">You can find her from stream archives, etc by visiting her socials below!</p>
+                <div class="mt-2 mb-16">
+                    <div v-for="social in socials.links" :class="`inline text-snow-light`">
+                        <a
+                            :href="social.link"
+                            class="btn block md:inline-block lg:inline-block px-5 py-3 mr-2 mb-2 hover:-translate-y-2 active:translate-y-1 active:translate-x-1 transition-all"
+                            :style="`background: ${social.color};`"
+                        >
+                            <Icon size="26" :name="social.icon" /> {{ social.username }}
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -45,9 +69,15 @@ useHead({
 const { data, pending, error }: any = await getData('overview');
 const friendshipInit: any = await getData('friendship');
 const triviaInit: any = await getData('trivia');
+const socialsInit: any = await getData('social');
 const oview = data.value?.data?.attributes;
 const frnds = friendshipInit.data.value?.data?.attributes;
 const trvia = triviaInit.data.value?.data?.attributes;
+const socials = socialsInit.data.value?.data?.attributes;
+const localePath = useLocalePath();
+const handleClick = (to: string) => {
+    location.href = to;
+};
 </script>
 
 <style></style>
